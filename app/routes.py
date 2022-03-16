20e1cb592de40d03721af2be120b0487625467ba
sin0nim <sin_it_syn@mail.ru>
2022-03-16 22:09:34 +0700

    Create migrations repository

---
+++ app/routes.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+from app import app
+from flask import render_template, flash, redirect, url_for
+from app.forms import LoginForm
+
+@app.route('/')
+@app.route('/index')
+def index():
+    user = {'username': 'Юрий Синицын'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        },
+        {
+            'author': {'username': 'Остап Бендер'},
+            'body': 'Грузите апельсины бочками!\nБратья Карамазовы'
+        },
+        {
+            'author': {'username': 'Олег Митяев'},
+            'body': 'Как здОрово, что все мы здесь\nСегодня собрались'
+        }
+    ]
+
+    return render_template('index.html', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html', title='Sign In', form=form)
